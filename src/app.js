2dfe32e1c821054fb1c3d93f3e82cacfd125f44a
tikkuk <karantikku7@gmail.com>
2018-11-04 16:52:59 +0530

    boilerplate for coindesk

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Test = () => (
+    <div>
+        Boilerplate for coindesk
+    </div>
+);
+
+ReactDOM.render(<Test />, document.getElementById("app"));
